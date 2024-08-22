db824efdf292538348d4fc37087dd3df1a2a6c69
Pedro Peña <pniaps@gmail.com>
2024-08-22 02:36:15 +0200

    Progress

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a==0 || b==0 || c==0
+    raise TriangleError.new("A triangle should not have any sides of length 0. If it does, it's either a line segment or a point, depending on how many sides are 0.")
+  end
+  if a<0 || b<0 || c<0
+    raise TriangleError.new("Negative length doesn't make sense.")
+  end
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError.new("Any two sides of a triangle should add up to more than the third side.")
+  end
+  if a == b and b == c
+    :equilateral
+  else
+    if a == b or b == c or a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
